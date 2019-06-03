2e1e9f57721d8bb98035088c5ea099dac1bbe423
zhoupeng <target_on@126.com>
2019-06-03 17:18:49 +0800

    change 1.TwoSum test

--- easy/1.TwoSum/solution_test.go
+++ easy/1.TwoSum/solution_test.go
@@ -1,15 +1,30 @@
 package solution
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestTwoSum(t *testing.T) {
-	nums, target := []int{2, 3, 2, 7, 11, 15}, 9
-	excepted := []int{0, 3}
-
-	output := twoSum(nums, target)
-	for i := range excepted {
-		if output[i] != excepted[i] {
-			t.Errorf("\n nums: %v, target: %v\n excepted: %v\n output: %v \n", nums, target, excepted, output)
-		}
+func Test_twoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{"test01", args{[]int{2, 3, 12, 5, 10, 7, 8}, 9}, []int{0, 5}},
+		{"test02", args{[]int{2, 3, 12, 5, 10, 7, 8}, 22}, []int{2, 4}},
+		{"test03", args{[]int{2, 3, 4, 5, 6, 7, 8, 11}, 11}, []int{3, 4}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
