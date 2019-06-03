2e1276dbda8424bb5dbffd5e6257121c7cdff241
zhoupeng <target_on@126.com>
2019-06-03 23:03:35 +0800

    sliding window for 3.LongestSubstringWithoutRepeatingCharacter

--- medium/3.LongestSubstringWithoutRepeatingCharacters/solution.go
+++ medium/3.LongestSubstringWithoutRepeatingCharacters/solution.go
@@ -1,15 +1,14 @@
 package solution
 
+import "strings"
+
 func lengthOfLongestSubstring(s string) int {
 	i, j, result, length := 0, 0, 0, len(s)
 	for i = 0; i < length; i++ {
-		cache := make(map[byte]byte)
-		cache[s[i]] = 1
 		for j = i + 1; j < length; j++ {
-			if _, ok := cache[s[j]]; ok {
+			if strings.Contains(s[i:j], s[j:j+1]) {
 				break
 			}
-			cache[s[j]] = 1
 		}
 		if result < (j - i) {
 			result = j - i
