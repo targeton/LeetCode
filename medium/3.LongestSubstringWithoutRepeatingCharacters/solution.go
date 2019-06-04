ca6346c285e4abc794c09fdb681cf2e1597219b1
zhoupeng <target_on@126.com>
2019-06-04 10:59:01 +0800

    Time Complexity:O(n) for Problem3

--- medium/3.LongestSubstringWithoutRepeatingCharacters/solution.go
+++ medium/3.LongestSubstringWithoutRepeatingCharacters/solution.go
@@ -1,17 +1,21 @@
 package solution
 
-import "strings"
-
 func lengthOfLongestSubstring(s string) int {
-	i, j, result, length := 0, 0, 0, len(s)
-	for i = 0; i < length; i++ {
-		for j = i + 1; j < length; j++ {
-			if strings.Contains(s[i:j], s[j:j+1]) {
-				break
+	i, result, length := 0, 0, len(s)
+	cache := make(map[byte]int)
+	for j := 0; j < length; j++ {
+		if pos, ok := cache[s[j]]; ok {
+			if i < pos {
+				i = pos
 			}
 		}
-		if result < (j - i) {
-			result = j - i
+
+		if result < j+1-i {
+			result = j + 1 - i
+		}
+		cache[s[j]] = j + 1
+		if j >= length-1 {
+			break
 		}
 	}
 	return result
--- medium/3.LongestSubstringWithoutRepeatingCharacters/solution_test.go
+++ medium/3.LongestSubstringWithoutRepeatingCharacters/solution_test.go
@@ -16,6 +16,9 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		{"test02", args{"bbbbb"}, 1},
 		{"test03", args{"pwwkew"}, 3},
 		{"test04", args{"abcabcdb"}, 4},
+		{"test05", args{"ab"}, 2},
+		{"test06", args{"a"}, 1},
+		{"test07", args{"aa"}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
