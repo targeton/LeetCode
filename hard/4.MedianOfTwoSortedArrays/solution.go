94bbbedc65f661b80ecb25ce8b77ef079ad20b1d
zhoupeng <target_on@126.com>
2019-06-04 21:20:50 +0800

    better method for handling boundary conditions

--- hard/4.MedianOfTwoSortedArrays/solution.go
+++ hard/4.MedianOfTwoSortedArrays/solution.go
@@ -4,9 +4,7 @@ import "math"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	if len(nums1) > len(nums2) {
-		temp := nums1
-		nums1 = nums2
-		nums2 = temp
+		nums1, nums2 = nums2, nums1
 	}
 	m, n := len(nums1), len(nums2)
 	if m == 0 {
@@ -14,54 +12,37 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 			return 0.0
 		} else if n%2 == 0 {
 			return (float64(nums2[n/2]) + float64(nums2[n/2-1])) / 2.0
+		} else {
+			return float64(nums2[n/2])
 		}
-		return float64(nums2[n/2])
 	}
-	imin, imax := 0, m
+	imin, imax, half := 0, m, (m+n+1)/2
 	var i, j int
 	maxL, minR := 0.0, 0.0
 	for {
 		i = (imin + imax) / 2
-		j = (m+n+1)/2 - i
-		if i == 0 {
-			if nums1[i] >= nums2[j-1] {
-				maxL = float64(nums2[j-1])
-				if j < n {
-					minR = math.Min(float64(nums1[i]), float64(nums2[j]))
-				} else {
-					minR = float64(nums1[i])
-				}
-				break
-			} else {
-				imin = i + 1
-				continue
-			}
-		}
-		if i == m {
-			if nums2[j] >= nums1[i-1] {
-				if j-1 >= 0 {
-					maxL = math.Max(float64(nums1[i-1]), float64(nums2[j-1]))
-				} else {
-					maxL = float64(nums1[i-1])
-				}
-				minR = float64(nums2[j])
-				break
-			} else {
-				imax = i - 1
-				continue
-			}
-		}
-		if nums1[i-1] <= nums2[j] && nums2[j-1] <= nums1[i] {
-			maxL = math.Max(float64(nums1[i-1]), float64(nums2[j-1]))
-			minR = math.Min(float64(nums1[i]), float64(nums2[j]))
-			break
-		}
-
-		if nums1[i-1] >= nums2[j] {
-			imax = i - 1
-		}
-		if nums2[j-1] >= nums1[i] {
+		j = half - i
+		if i < m && nums2[j-1] > nums1[i] {
 			imin = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			imax = i - 1
+		} else {
+			if i == 0 {
+				maxL = float64(nums2[j-1])
+			} else if j == 0 {
+				maxL = float64(nums1[i-1])
+			} else {
+				maxL = math.Max(float64(nums2[j-1]), float64(nums1[i-1]))
+			}
+
+			if i == m {
+				minR = float64(nums2[j])
+			} else if j == n {
+				minR = float64(nums1[i])
+			} else {
+				minR = math.Min(float64(nums2[j]), float64(nums1[i]))
+			}
+			break
 		}
 	}
 	if (m+n)%2 == 1 {
