240694ed18e017a6eb65d9db286fba5b7760ba81
zhoupeng <target_on@126.com>
2019-06-10 15:09:09 +0800

    Use Binest Method(one metrix solution) to calculate fibonacci numbers.Time complexity:O(log(n)), space complexity:O(1)

--- other/02.FibonacciNumber/solution.go
+++ other/02.FibonacciNumber/solution.go
@@ -4,24 +4,41 @@ func fib(n int) int {
 	if n < 2 {
 		return n
 	}
-	first := 0
-	second := 1
-	for i := 2; i <= n; i++ {
-		third := first + second
-		first, second = second, third
-	}
-	return second
+	a := [][]int{{1, 1}, {1, 0}}
+	r := pow(a, n-1)
+	return r[0][0]
 }
 
 func climbStairs(n int) int {
-	if n < 2 {
-		return n
+	a := [][]int{{1, 1}, {1, 0}}
+	r := pow(a, n)
+	return r[0][0]
+}
+
+func pow(a [][]int, n int) [][]int {
+	ret := [][]int{{1, 0}, {0, 1}}
+	for {
+		if n <= 0 {
+			break
+		}
+		if n&1 == 1 {
+			ret = multiply(ret, a)
+		}
+		n >>= 1
+		a = multiply(a, a)
 	}
-	first := 1
-	second := 2
-	for i := 3; i <= n; i++ {
-		third := first + second
-		first, second = second, third
+	return ret
+}
+
+func multiply(a [][]int, b [][]int) [][]int {
+	var result = make([][]int, 2)
+	for i := 0; i < 2; i++ {
+		result[i] = make([]int, 2)
 	}
-	return second
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			result[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j]
+		}
+	}
+	return result
 }
