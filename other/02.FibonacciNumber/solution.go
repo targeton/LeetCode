dffd3543a93d4a8786eacc4b3fc674d0c962837c
zhoupeng <target_on@126.com>
2019-06-10 14:45:52 +0800

    Use dynamic programming to calculate fibonacci numbers.Time complexity:O(n), space complexity:O(1)

--- other/02.FibonacciNumber/solution.go
+++ other/02.FibonacciNumber/solution.go
@@ -1,30 +1,27 @@
 package solution
 
-var fibCache = make(map[int]int)
-var cache = make(map[int]int)
-
 func fib(n int) int {
-	if old, ok := fibCache[n]; ok {
-		return old
-	}
 	if n < 2 {
-		fibCache[n] = n
 		return n
 	}
-	result := fib(n-1) + fib(n-2)
-	fibCache[n] = result
-	return result
+	first := 0
+	second := 1
+	for i := 2; i <= n; i++ {
+		third := first + second
+		first, second = second, third
+	}
+	return second
 }
 
 func climbStairs(n int) int {
-	if old, ok := cache[n]; ok {
-		return old
-	}
-	if n <= 2 {
-		cache[n] = n
+	if n < 2 {
 		return n
 	}
-	result := climbStairs(n-1) + climbStairs(n-2)
-	cache[n] = result
-	return result
+	first := 1
+	second := 2
+	for i := 3; i <= n; i++ {
+		third := first + second
+		first, second = second, third
+	}
+	return second
 }
