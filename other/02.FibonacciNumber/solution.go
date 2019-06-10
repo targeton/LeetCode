0045b9bf0f411eb5620fe11adc4e8cdb0bebc649
zhoupeng <target_on@126.com>
2019-06-10 14:39:08 +0800

    Use recursion(memoization) to calculate fibonacci numbers.Time complexity:O(n), space complexity:O(n)

--- other/02.FibonacciNumber/solution.go
+++ other/02.FibonacciNumber/solution.go
@@ -1,15 +1,30 @@
 package solution
 
+var fibCache = make(map[int]int)
+var cache = make(map[int]int)
+
 func fib(n int) int {
+	if old, ok := fibCache[n]; ok {
+		return old
+	}
 	if n < 2 {
+		fibCache[n] = n
 		return n
 	}
-	return fib(n-1) + fib(n-2)
+	result := fib(n-1) + fib(n-2)
+	fibCache[n] = result
+	return result
 }
 
 func climbStairs(n int) int {
+	if old, ok := cache[n]; ok {
+		return old
+	}
 	if n <= 2 {
+		cache[n] = n
 		return n
 	}
-	return climbStairs(n-1) + climbStairs(n-2)
+	result := climbStairs(n-1) + climbStairs(n-2)
+	cache[n] = result
+	return result
 }
