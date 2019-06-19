000f91ae117b8650080c8dfd13532cac1b5c8efc
zhoupeng <target_on@126.com>
2019-06-20 00:05:21 +0800

    solution for Binary Tree Inorder Traversal(Recursive)

--- medium/94.BinaryTreeInorderTraversal/solution.go
+++ medium/94.BinaryTreeInorderTraversal/solution.go
@@ -9,17 +9,11 @@ type TreeNode struct {
 
 func inorderTraversal(root *TreeNode) []int {
 	var result []int
-	var stack []*TreeNode
-	cur := root
-	for cur != nil || len(stack) > 0 {
-		for cur != nil {
-			stack = append(stack, cur)
-			cur = cur.Left
-		}
-		last := stack[len(stack)-1]
-		result = append(result, last.Val)
-		stack = stack[:len(stack)-1]
-		cur = last.Right
+	if root == nil {
+		return result
 	}
+	result = append(result, inorderTraversal(root.Left)...)
+	result = append(result, root.Val)
+	result = append(result, inorderTraversal(root.Right)...)
 	return result
 }
