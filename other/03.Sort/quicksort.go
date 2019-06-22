7e867da107a2b2f20d56dfc7f93771272299a24d
zhoupeng <target_on@126.com>
2019-06-22 17:13:17 +0800

    two more implemention of quicksort (by sliding window, divide and conquer

--- other/03.Sort/quicksort.go
+++ other/03.Sort/quicksort.go
@@ -2,11 +2,14 @@ package sort
 
 func sort(n []int) []int {
 	//quicksort
-	quicksort(n, 0, len(n)-1)
+	// quicksort1(n, 0, len(n)-1)
+	// quicksort2(n, 0, len(n)-1)
+	quicksort3(n)
 	return n
 }
 
-func quicksort(n []int, left int, right int) {
+//
+func quicksort1(n []int, left int, right int) {
 	p := left
 	i, j := left, right
 	for i <= j {
@@ -26,9 +29,48 @@ func quicksort(n []int, left int, right int) {
 		}
 	}
 	if p-left > 1 {
-		quicksort(n, left, p-1)
+		quicksort1(n, left, p-1)
 	}
 	if right-p > 1 {
-		quicksort(n, p+1, right)
+		quicksort1(n, p+1, right)
 	}
+}
+
+func quicksort2(n []int, left int, right int) {
+	if left >= right {
+		return
+	}
+	divider := left
+	p := right
+	for i := left + 1; i <= p; {
+		if n[i] >= n[divider] {
+			n[i], n[p] = n[p], n[i]
+			p--
+		} else {
+			n[i], n[divider] = n[divider], n[i]
+			i++
+			divider++
+		}
+	}
+	quicksort2(n, left, divider-1)
+	quicksort2(n, divider+1, right)
+}
+
+func quicksort3(n []int) {
+	if len(n) <= 1 {
+		return
+	}
+	head, tail, i := 0, len(n)-1, 1
+	for head < tail {
+		if n[i] > n[head] {
+			n[i], n[tail] = n[tail], n[i]
+			tail--
+		} else {
+			n[i], n[head] = n[head], n[i]
+			i++
+			head++
+		}
+	}
+	quicksort3(n[:head])
+	quicksort3(n[head+1:])
 }
--- other/03.Sort/quicksort_test.go
+++ other/03.Sort/quicksort_test.go
@@ -20,6 +20,7 @@ func Test_sort(t *testing.T) {
 		{"test03", args{[]int{5, 6, 4, 9, 3, 2, 7, 1, 8}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
 		{"test04", args{[]int{0, 1, 0, 4, 0, 2, 3}}, []int{0, 0, 0, 1, 2, 3, 4}},
 		{"test05", args{[]int{1, 2, 1, 3, 2, 3, 4}}, []int{1, 1, 2, 2, 3, 3, 4}},
+		{"test06", args{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
