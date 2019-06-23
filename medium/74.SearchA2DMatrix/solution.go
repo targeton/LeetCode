201f06d058d16f34416332a36103332dd2a7ac2f
zhoupeng <target_on@126.com>
2019-06-23 17:22:58 +0800

    simplify code for binary search

--- medium/74.SearchA2DMatrix/solution.go
+++ medium/74.SearchA2DMatrix/solution.go
@@ -1,32 +1,19 @@
 package solution
 
 func searchMatrix(matrix [][]int, target int) bool {
-	if matrix == nil || len(matrix) < 1 || len(matrix[0]) < 1 {
+	if matrix == nil || len(matrix) < 1 {
 		return false
 	}
-	i, j := 0, len(matrix)-1
+	m, n := len(matrix), len(matrix[0])
+	i, j := 0, m*n-1
 	for i <= j {
 		k := (i + j) / 2
-		if matrix[k][0] == target {
+		if matrix[k/n][k%n] == target {
 			return true
-		} else if matrix[k][0] > target {
+		} else if matrix[k/n][k%n] > target {
 			j = k - 1
 		} else {
 			i = k + 1
-		}
-	}
-	if i == 0 {
-		return false
-	}
-	p, q := 0, len(matrix[i-1])-1
-	for p <= q {
-		r := (p + q) / 2
-		if matrix[i-1][r] == target {
-			return true
-		} else if matrix[i-1][r] > target {
-			q = r - 1
-		} else {
-			p = r + 1
 		}
 	}
 	return false
