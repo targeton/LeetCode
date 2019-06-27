571bf66eb4d418b486641cfff55848104ee7ebb4
zhoupeng <target_on@126.com>
2019-06-27 20:23:21 +0800

    dynamic programming to solve 10. regular expression matching. Time complexity: O(SP), Space complexity: O(SP).

--- hard/10.RegularExpressionMatching/solution.go
+++ hard/10.RegularExpressionMatching/solution.go
@@ -1,12 +1,25 @@
 package solution
 
 func isMatch(s string, p string) bool {
-	if len(p) < 1 {
-		return len(s) < 1
+	cache := make([][]bool, len(s)+1)
+	for i := 0; i < len(s)+1; i++ {
+		cache[i] = make([]bool, len(p)+1)
 	}
-	first := len(s) > 0 && (s[0] == p[0] || p[0] == '.')
-	if len(p) > 1 && p[1] == '*' {
-		return first && isMatch(s[1:], p) || isMatch(s, p[2:])
+	// cache[len(s)][len(p)] means s == "", p == ""
+	// cache[i][j] means s[i:],p[j:]
+	for i := len(s); i >= 0; i-- {
+		for j := len(p); j >= 0; j-- {
+			if j == len(p) {
+				cache[i][j] = i == len(s)
+			} else {
+				first := i < len(s) && (s[i] == p[j] || p[j] == '.')
+				if j+1 < len(p) && p[j+1] == '*' {
+					cache[i][j] = first && cache[i+1][j] || cache[i][j+2]
+				} else {
+					cache[i][j] = first && cache[i+1][j+1]
+				}
+			}
+		}
 	}
-	return first && isMatch(s[1:], p[1:])
+	return cache[0][0]
 }
