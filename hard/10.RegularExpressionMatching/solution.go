97da8995cdf17bcb73cbe4bc4cdca0a80d419602
zhoupeng <target_on@126.com>
2019-06-27 21:14:13 +0800

    use recursive(with cache to avoid duplicate computation)

--- hard/10.RegularExpressionMatching/solution.go
+++ hard/10.RegularExpressionMatching/solution.go
@@ -1,25 +1,34 @@
 package solution
 
+var cache [][]int
+
 func isMatch(s string, p string) bool {
-	cache := make([][]bool, len(s)+1)
+	cache = make([][]int, len(s)+1)
 	for i := 0; i < len(s)+1; i++ {
-		cache[i] = make([]bool, len(p)+1)
+		cache[i] = make([]int, len(p)+1)
 	}
-	// cache[len(s)][len(p)] means s == "", p == ""
-	// cache[i][j] means s[i:],p[j:]
-	for i := len(s); i >= 0; i-- {
-		for j := len(p); j >= 0; j-- {
-			if j == len(p) {
-				cache[i][j] = i == len(s)
-			} else {
-				first := i < len(s) && (s[i] == p[j] || p[j] == '.')
-				if j+1 < len(p) && p[j+1] == '*' {
-					cache[i][j] = first && cache[i+1][j] || cache[i][j+2]
-				} else {
-					cache[i][j] = first && cache[i+1][j+1]
-				}
-			}
+	return dp(0, 0, s, p)
+}
+
+func dp(i int, j int, s string, p string) bool {
+	if cache[i][j] != 0 {
+		return cache[i][j] > 0
+	}
+	var ans bool
+	if j == len(p) {
+		ans = i == len(s)
+	} else {
+		first := i < len(s) && (s[i] == p[j] || p[j] == '.')
+		if j+1 < len(p) && p[j+1] == '*' {
+			ans = first && dp(i+1, j, s, p) || dp(i, j+2, s, p)
+		} else {
+			ans = first && dp(i+1, j+1, s, p)
 		}
 	}
-	return cache[0][0]
+	if ans {
+		cache[i][j] = 1
+	} else {
+		cache[i][j] = -1
+	}
+	return ans
 }
